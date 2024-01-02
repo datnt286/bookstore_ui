6a5ef74c4fa58859632a67c639b6a966bdea92ff
datnt286 <datnt286@gmail.com>
2024-01-03 00:21:26 +0700

    fix newsletter4

---
+++ src/components/Newsletter/index.js
@@ -0,0 +1,30 @@
+function Newsletter() {
+    return (
+        <div id="newsletter" className="section">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="newsletter">
+                            <p>
+                                <strong>Đăng ký</strong> để nhận những thông báo mới 
+                            </p>
+                            <form>
+                                <input
+                                    id="newsletter-input"
+                                    className="form-control input"
+                                    type="email"
+                                    placeholder="Nhập email của bạn"
+                                />
+                                <button id="newsletter-btn" className="newsletter-btn">
+                                    <i className="fa fa-envelope"></i> Đăng ký
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Newsletter;
