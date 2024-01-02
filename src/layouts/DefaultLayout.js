cd2c3653f490cf439cd45444c8c83643dfa55c62
datnt286 <datnt286@gmail.com>
2024-01-03 00:17:55 +0700

    fix newsletter3

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -1,13 +1,11 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Newsletter from '../components/Newsletter';
 
 function DefaultLayout({ children }) {
     return (
         <>
             <Header />
             <main>{children}</main>
-            <Newsletter />
             <Footer />
         </>
     );
