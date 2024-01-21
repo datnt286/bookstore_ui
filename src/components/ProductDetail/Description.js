05c18813279d94bb15c954ad0ff69f5f3d59491a
datnt286 <datnt286@gmail.com>
2024-01-22 01:50:26 +0700

    fix ui footer

--- src/components/ProductDetail/Description.js
+++ src/components/ProductDetail/Description.js
@@ -3,17 +3,7 @@ function Description({ data }) {
         <div id="description" className="tab-pane fade in active">
             <div className="row">
                 <div className="col-md-12">
-                    {data.description ? (
-                        <p>{data.description}</p>
-                    ) : (
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </p>
-                    )}
+                    <p>{data.description || 'Hiện chưa có mô tả'}</p>
                 </div>
             </div>
         </div>
